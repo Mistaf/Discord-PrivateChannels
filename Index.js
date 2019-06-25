005336d7ef8e8def2d5f4cfae4d4ee17ba290270
Mistaf <hi@mistaf.net>
2019-06-25 04:41:59 +0200

    added create invite kick and help commands

--- Index.js
+++ Index.js
@@ -1,16 +1,60 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./config.json");
-bot.login(config.token);
+
+bot.login(config.Token);
 /**
  * logs in the console when the bot is on
  */
 bot.on('ready',() => {
     console.log("Bot is ready!");
+    require('./private.js').ready(bot);
+    bot.user.setActivity("/privatehelp for help",{type:"watching"});
 });
 
 
 bot.on("message",message=>{
     let args = message.content.toLowerCase().split(' ');
+    if(message.channel.type!="text"||message.author.bot)return;
+
+    if(!message.guild.me.hasPermission("ADMINISTRATOR")){
+        message.channel.send("I do not have the permissions to manage channels!\nPlease make sure the bot is Admin");
+        return;
+    }
+    switch (args[0]) {
+        case "/privatecreate":
+            require('./private.js').create(message,bot);  
+            break;
+        case "/privatekick":
+            require('./private.js').kick(message);  
+            break;
+        case "/privateinvite":
+            require('./private.js').invite(message);  
+            break;
+        case "/privatehelp":
+            sendHelp(message);
+            break;
+        default:
+            break;
+    }
 
 });
+/**
+ * Delete the channel if nobody is in it anymore
+ */
+bot.on("voiceStateUpdate",(oldMember,newMember)=>{
+    if(!oldMember.guild.me.hasPermission("MANAGE_CHANNELS")||!oldMember.guild.me.hasPermission("MOVE_MEMBERS"))return;
+    if(oldMember.voiceChannel&&oldMember.voiceChannel.name.includes(' Private Channel')&&oldMember.voiceChannel!=newMember.voiceChannel&&oldMember.voiceChannel.members.array().length==0)oldMember.voiceChannel.delete();
+});
+
+function sendHelp(message){
+
+    var help = new Discord.RichEmbed()
+        .setTitle("Hey "+message.author.username+"!")
+        .setDescription("Here is everything I can do!")
+        .addField("/privatecreate","Creates a private channel for you and the people you invite!")
+        .addField("/privateinvite","Invite a person from the same discord server to your private channel by tagging them")
+        .addField("/privatekick","Kick the person from your private channel")
+        .setColor('#'+Math.random().toString(16).slice(2, 8));
+    message.author.send(help);
+}
---
+++ private.js
@@ -0,0 +1,146 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+exports.create = async function (message) {
+    let guild=message.guild;
+    let everyone = guild.roles.find(role=>role.name==="@everyone");
+    let parent = guild.channels.find(channel=>channel.name==="Private Channels");
+    if(parent==null){await createParent(guild).then(function(result){parent=result})}
+    let papa=guild.channels.get(parent.id).children.array();
+    if(papa){
+        for(i=0;i<papa.length;i++){
+            if(papa[i].name==message.author.id+' Private Channel'){
+                message.reply("You can only have 1 private channel at the time").then(function(m){deletemsg(m);});
+                return;
+            }
+        }
+    }
+    guild.createChannel(message.author.id+' Private Channel', {
+        type: 'voice',
+        parent:parent.id,
+        permissionOverwrites: [{
+            id: message.author.id,
+            allow:["CONNECT"]
+        },
+        {
+            id: everyone,
+            deny: ["CONNECT"]
+        }]
+    }).then(function(m){
+            runTimer();
+            function runTimer() {
+                setTimeout(function () {
+                    //delete if possible
+                    if(m.deleteable)m.delete();
+                    let rmvchannel =guild.channels.find(channel=>channel.name===message.author.id+' Private Channel');
+                    if(rmvchannel){
+                        //remove channel if nobody joined after 10 seconds
+                        if(rmvchannel.members.array().length==0)rmvchannel.delete();
+                    }
+                    
+                }, 10000);
+            }
+        })
+        .catch(console.error);
+
+}
+
+exports.invite = function (message) {
+    
+    let guild=message.guild;
+    let parent = guild.channels.find(channel=>channel.name==="Private Channels");
+    if(parent==null){
+        message.reply("Make sure you got an private channel").then(function(m){deletemsg(m);});
+        return;
+    }
+    let papa=guild.channels.get(parent.id).children.array();
+    if(papa){
+        for(i=0;i<papa.length;i++){
+            if(papa[i].name==message.author.id+' Private Channel'){
+
+            let userp=message.mentions.users.first();
+            if(userp==null||userp==undefined){
+                userp=message.content.split(" ")[1]
+            }else{
+                userp=userp.id;
+            }
+            let target=message.guild.members.get(userp);
+            if(target==undefined){
+                message.channel.send("Cannot invite that person make sure u typed everything correctly").then(function(m){
+                deletemsg(m);
+                });return;
+            }
+            guild.channels.find(channel=>channel.name===message.author.id+' Private Channel').overwritePermissions(target.id,{'CONNECT':true});
+            message.channel.send(target+' you have been invited the private channel of '+message.author);
+            return;}
+        }
+        message.reply("Make sure you got an private channel").then(function(m){deletemsg(m);});
+    }
+}
+
+exports.kick = function (message) {
+    if(!message)return;
+    if(!message.member.voiceChannel){
+        message.reply("Join your voicechannel before kicking the person. You haven't even talked to him yet...").then(function(m){deletemsg(m);})
+        return;
+    }
+    if(message.member.voiceChannel.name==message.author.id+" Private Channel"){
+        let voiceChannel=message.member.voiceChannel;
+        let mentionedUser=message.mentions.users.first();
+        if(mentionedUser==null||mentionedUser==undefined){
+            //makes is so you could use there discord id instead of tagging
+            mentionedUser=message.content.split(" ")[1];
+        }else{
+            mentionedUser=mentionedUser.id;
+        }
+        let target=message.guild.members.get(mentionedUser);
+        if(target==undefined){
+            message.channel.send("Cannot remove that person make sure u typed everything correctly").then(function(m){deletemsg(m);});
+            return;
+        }
+  
+        voiceChannel.overwritePermissions(target.id,{'CONNECT':false});
+
+        if(target.voiceChannel&&target.voiceChannel.name==message.author.id+" Private Channel")target.setVoiceChannel(null);
+        else message.reply("This creature isn't even in your channel...").then(function(m){deletemsg(m);})
+      }else{
+          message.reply("Do you even own a private voicechannel???").then(function(m){deletemsg(m);})
+      }
+      
+}
+
+
+
+exports.ready = function (bot) {
+    let guilds = bot.guilds.array();
+    guilds.forEach(guild => {
+        let parent = guild.channels.find(channel=>channel.name==="Private Channels");
+        if(parent==null)return;
+        let privates=guild.channels.get(parent.id).children.array();
+        if(privates){
+          for(i=0;i<privates.length;i++){
+            if(privates[i].name.includes(' Private Channel')&&privates[i].members.array().length==0){
+              privates[i].delete();
+              return;
+            }
+          }
+        }
+    });
+}
+function createParent(guild){
+    return guild.createChannel('Private Channels', {
+        type: 'category',
+        permissionOverwrites: [{
+          id: guild.id,
+          deny: ['CONNECT']
+        }]
+      })
+      .then(parent=>{return parent})
+      .catch(console.error);
+
+}
+function deletemsg(m) {
+    setTimeout(function () {
+        if(m.deleteable)m.delete();
+    }, 6000);
+}
